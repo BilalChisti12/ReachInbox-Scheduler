493faa3178f625db5887f3f310cb0d7646c5e290
Mohammed Bilal Sharief <bmohamme3@gitam.in>
2026-09-08 11:12:41 +0530

    Delete misc

--- backend/revertPorts.js
+++
@@ -1,8 +0,0 @@
-﻿
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-p.sender.updateMany({
-  where: { smtpHost: "smtp.ethereal.email" },
-  data: { smtpPort: 587 }
-}).then(console.log).finally(() => p.$disconnect());
-
